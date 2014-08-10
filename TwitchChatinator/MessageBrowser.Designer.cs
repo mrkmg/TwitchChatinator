8d7374feb9a5b9869e2d9e13f0938359a7359c91
Kevin Gravier <kevin@mrkmg.com>
2014-08-10 04:29:35 -0400

    Got a lot of stuff working... Still learning

---
+++ TwitchChatinator/MessageBrowser.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace TwitchChatinator
+{
+    partial class MessageBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MessagesList = new System.Windows.Forms.DataGridView();
+            this.GetDataButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MessagesList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MessagesList
+            // 
+            this.MessagesList.AllowUserToAddRows = false;
+            this.MessagesList.AllowUserToDeleteRows = false;
+            this.MessagesList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MessagesList.Location = new System.Drawing.Point(0, 45);
+            this.MessagesList.Name = "MessagesList";
+            this.MessagesList.ReadOnly = true;
+            this.MessagesList.Size = new System.Drawing.Size(901, 364);
+            this.MessagesList.TabIndex = 0;
+            // 
+            // GetDataButton
+            // 
+            this.GetDataButton.Location = new System.Drawing.Point(13, 13);
+            this.GetDataButton.Name = "GetDataButton";
+            this.GetDataButton.Size = new System.Drawing.Size(75, 23);
+            this.GetDataButton.TabIndex = 1;
+            this.GetDataButton.Text = "Get DATA";
+            this.GetDataButton.UseVisualStyleBackColor = true;
+            this.GetDataButton.Click += new System.EventHandler(this.GetDataButton_Click);
+            // 
+            // MessageBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 392);
+            this.Controls.Add(this.GetDataButton);
+            this.Controls.Add(this.MessagesList);
+            this.Name = "MessageBrowser";
+            this.Text = "Message Browser";
+            ((System.ComponentModel.ISupportInitialize)(this.MessagesList)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView MessagesList;
+        private System.Windows.Forms.Button GetDataButton;
+
+    }
+}
---
+++ TwitchChatinator/SetCredentialsScreen.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TwitchChatinator
+{
+    partial class SetCredentialsScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.UsernameInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PasswordInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(36, 142);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 0;
+            this.LoginButton.Text = "Save";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // UsernameInput
+            // 
+            this.UsernameInput.Location = new System.Drawing.Point(38, 39);
+            this.UsernameInput.Name = "UsernameInput";
+            this.UsernameInput.Size = new System.Drawing.Size(145, 20);
+            this.UsernameInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Twitch Username";
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.Location = new System.Drawing.Point(38, 92);
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.Size = new System.Drawing.Size(145, 20);
+            this.PasswordInput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Twitch OAuth Token";
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(110, 142);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 5;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // SetCredentialsScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 186);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UsernameInput);
+            this.Controls.Add(this.LoginButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "SetCredentialsScreen";
+            this.Text = "Set Credentials";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox UsernameInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PasswordInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
