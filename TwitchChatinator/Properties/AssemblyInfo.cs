bdf9acb803602213f682c419dc0dbd219e6aef96
Kevin Gravier <kevin@mrkmg.com>
2014-08-13 01:14:31 -0400

    Simple Changes

--- TwitchChatinator/Properties/AssemblyInfo.cs
+++ TwitchChatinator/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TwitchChatinator")]
+[assembly: AssemblyTitle("Chatinator for Twitch")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kevin Gravier")]
 [assembly: AssemblyProduct("TwitchChatinator")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
